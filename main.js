b35cc4b11555b1e3340af7ea8f206a1257a48bcd
Allohamora <herman2000@yandex.ru>
2020-02-26 21:30:40 +0200

    Swapped functionality
        * main.js
    
            * swapped buttonRows onclick with buttonSquares onclick

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const buttonRows = document.querySelector(".buttons .button-rows");
 const buttonSquares = document.querySelector(".buttons .button-squares");
 const flexContainer = document.querySelector(".flex-container");
 
-buttonRows.onclick = () => flexContainer.classList.remove("column");
-buttonSquares.onclick = () => flexContainer.classList.add("column"); 
+buttonRows.onclick = () => flexContainer.classList.add("column"); 
+buttonSquares.onclick = () => flexContainer.classList.remove("column")
